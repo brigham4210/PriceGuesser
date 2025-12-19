f82d4891af693df0f0ef5e556b2e87879523dcf0
Brigham Chen <brigham.chen@familysearch.org>
2025-12-18 21:02:54 -0500

    Update properties.py

--- Scraper/properties.py
+++ Scraper/properties.py
@@ -1,21 +1,35 @@
+import requests
 from bs4 import BeautifulSoup
+
+from Scraper.url import TEST_URL
 
 
 class Properties:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, website_url):
+        self.url = website_url
+        self.session = requests.Session()
 
     def property_urls(self):
-        soup = BeautifulSoup(self.url, 'html.parser')
-        property_list_section = soup.find('section', {'data-testid': 'property-list'})
+        headers = {
+            "User-Agent": "Mozilla/5.0 Chrome/120.0.0.0",
+            "Accept-Language": "en",
+            "Accept": "text/html",
+        }
+        response = self.session.get(self.url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
         property_urls = []
 
-        if property_list_section:
-            property_cards = property_list_section.find_all('div', {'data-testid': 'card-content'})
-
+        if soup.find('title').text != "Access to this page has been denied":
+            print("Access granted, scraping property URLs...")
+            property_cards = soup.find_all('a', {'data-test': 'property-card-link'})
             for card in property_cards:
-                link_tag = card.find('a', href=True)
-                if link_tag:
-                    property_urls.append(link_tag['href'])
+                property_urls.append(card['href'])
 
         return property_urls
+
+
+if __name__ == "__main__":
+    properties = Properties(TEST_URL)
+    urls = properties.property_urls()
+    for url in urls:
+        print(url)
